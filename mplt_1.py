40ba3c0cbfe5d02fcbbfa2ecd8d286bc22f3a3ab
Ramaev01 <160835128+Ramaev01@users.noreply.github.com>
2024-03-29 20:40:06 +0400

    Delete mplt_1.py

--- mplt_1.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-x = np.linspace(-10,10,1000)
-y = np.sin(2*x)**2*np.exp((x + 2)/10)**2
-plt.figure(figsize=(8,3))
-plt.grid(lw=0.5, ls='--')
-plt.plot(x, y, lw=4.0, color='red')
-plt.show()
